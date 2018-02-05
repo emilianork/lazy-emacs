d79c4f07e2238ccd6e225ca75fef9c0942b76fe1
Emiliano Cabrera <jemiliano.cabrera@gmail.com>
2018-02-05 13:06:20 -0600

    Fix LaTeX-mode
    It was taking the old latex-mode instead of using auctex LaTeX-mode.

--- layers/latex-layer/latex-layer.el
+++ layers/latex-layer/latex-layer.el
@@ -1,22 +1,44 @@
+;; Currently this is not working
+
 (defun lazy-emacs/latex-mode-load ()
   (interactive)
-  (if (not (featurep 'tex-site))
+  (if (not (featurep 'auctex))
       (progn
-        (lazy-emacs/require-package 'tex-site)
+        (lazy-emacs/require-package 'auctex 'preview-latex)
 
+        (TeX-load-hack)
+
+        (add-hook 'LaTeX-mode-hook 'visual-line-mode)
         (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
         (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
+        ;; Set column to 80
+        (add-hook 'LaTeX-mode-hook      'lazy-emacs/latex-column-80)
+        (add-hook 'plain-TeX-mode-hook  'lazy-emacs/latex-column-80)
+
         ;; Adds latexmk-dvi to the compilation commands list
-        (push '("latexmk-dvi" "latexmk %s && dvipdf %s" TeX-run-TeX nil t
-                :help "Run latexmk on file")
-              TeX-command-list)))
+        (LaTeX-mode)
+        (push '("latexmk-dvi" "latexmk %s && dvipdf %s" TeX-run-TeX nil t :help "Run latexmk on file")
+              TeX-command-list)
+
+        (add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk-dvi")))))
+
+  (LaTeX-mode)
 
   (setq TeX-auto-save t)
   (setq TeX-parse-self t)
   (setq TeX-PDF-mode t)
   (setq reftex-plug-into-AUCTeX t)
-  (setq TeX-command-default "latexmk-dvi"))
+  (setq TeX-command-default "latexmk-dvi")
+
+  (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
+  (setq TeX-view-program-list
+        '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b"))))
+
+
+(defun lazy-emacs/latex-column-80 ()
+  (turn-on-auto-fill)
+  (set-fill-column 80))
 
 (add-to-list 'auto-mode-alist '("\\.tex\\'" . lazy-emacs/latex-mode-load))
 (add-to-list 'auto-mode-alist '("\\.bib\\'" . lazy-emacs/latex-mode-load))
